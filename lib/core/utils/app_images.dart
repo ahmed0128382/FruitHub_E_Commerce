dee8f16df37fdb761194cf1edf687ca296a08ff4
ahmed ibrahim mousa <ahmed0128382@gmail.com>
2025-06-27 16:00:05 +0300

    make the splash view ui

---
+++ lib/core/utils/app_images.dart
@@ -0,0 +1,29 @@
+// ignore_for_file: prefer_single_quotes
+class Assets {
+  Assets._();
+  
+  /// Assets for imagesCircles
+  /// assets/images/Circles.svg
+  static const String imagesCircles = "assets/images/Circles.svg";
+
+  /// Assets for imagesFruitBasket
+  /// assets/images/fruitBasket.svg
+  static const String imagesFruitBasket = "assets/images/fruitBasket.svg";
+
+  /// Assets for imagesFruitHub
+  /// assets/images/FruitHub.svg
+  static const String imagesFruitHub = "assets/images/FruitHub.svg";
+
+  /// Assets for imagesFruitsIcon
+  /// assets/images/fruits_icon.png
+  static const String imagesFruitsIcon = "assets/images/fruits_icon.png";
+
+  /// Assets for imagesPineapple
+  /// assets/images/pineapple.svg
+  static const String imagesPineapple = "assets/images/pineapple.svg";
+
+  /// Assets for imagesPlantInject
+  /// assets/images/PlantInject.svg
+  static const String imagesPlantInject = "assets/images/PlantInject.svg";
+}
+
--- lib/features/splash/presentation/views/splash_view.dart
+++ lib/features/splash/presentation/views/splash_view.dart
@@ -1,10 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:fruit_hub/features/splash/presentation/views/splash_view_body.dart';
 
 class SplashView extends StatelessWidget {
   const SplashView({super.key});
   static const routeName = 'splash';
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      body: SplashViewBody(),
+    );
   }
 }
