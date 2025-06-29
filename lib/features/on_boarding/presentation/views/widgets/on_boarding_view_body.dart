11a7329ba70b4c0bb9b0e1ae2c1e2f2498400041
ahmed ibrahim mousa <ahmed0128382@gmail.com>
2025-06-29 19:40:20 +0300

    add the custom button widget

--- lib/features/on_boarding/presentation/views/widgets/on_boarding_view_body.dart
+++ lib/features/on_boarding/presentation/views/widgets/on_boarding_view_body.dart
@@ -1,4 +1,7 @@
+import 'package:dots_indicator/dots_indicator.dart';
 import 'package:flutter/material.dart';
+import 'package:fruit_hub/core/utils/app_colors.dart';
+import 'package:fruit_hub/features/on_boarding/presentation/views/widgets/custom_button.dart';
 import 'package:fruit_hub/features/on_boarding/presentation/views/widgets/on_boarding_page_view.dart';
 
 class OnBoardingViewBody extends StatelessWidget {
@@ -9,6 +12,19 @@ class OnBoardingViewBody extends StatelessWidget {
     return Column(
       children: [
         OnBoardingPageView(),
+        DotsIndicator(
+          dotsCount: 2,
+          decorator: DotsDecorator(
+              activeColor: AppColors.primaryColor,
+              color: AppColors.primaryColor.withValues(alpha: 0.5)),
+        ),
+        Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: CustomButton(
+            text: 'ابدأ الأن',
+            onTap: () {},
+          ),
+        )
       ],
     );
   }
