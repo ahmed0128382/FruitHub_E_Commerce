f4bf7d0864ed0c3ca71f4e5acda57e71afa30903
ahmed ibrahim mousa <ahmed0128382@gmail.com>
2025-06-30 23:18:50 +0300

    make the app text styles file

---
+++ lib/core/utils/app_styles.dart
@@ -0,0 +1,54 @@
+import 'package:flutter/material.dart';
+
+abstract class AppStyles {
+  static const TextStyle bold13 = TextStyle(
+    fontFamily: 'Cairo',
+    fontWeight: FontWeight.bold,
+    fontSize: 13,
+  );
+  static const TextStyle semiBold13 = TextStyle(
+    fontFamily: 'Cairo',
+    fontWeight: FontWeight.w600,
+    fontSize: 13,
+  );
+  static const TextStyle regular13 = TextStyle(
+    fontFamily: 'Cairo',
+    fontWeight: FontWeight.normal,
+    fontSize: 13,
+  );
+  static const TextStyle regular16 = TextStyle(
+    fontFamily: 'Cairo',
+    fontWeight: FontWeight.normal,
+    fontSize: 16,
+  );
+  static const TextStyle bold16 = TextStyle(
+    fontFamily: 'Cairo',
+    fontWeight: FontWeight.bold,
+    fontSize: 16,
+  );
+  static const TextStyle semiBold16 = TextStyle(
+    fontFamily: 'Cairo',
+    fontWeight: FontWeight.w600,
+    fontSize: 16,
+  );
+  static const TextStyle regular23 = TextStyle(
+    fontFamily: 'SF Pro Text',
+    fontWeight: FontWeight.normal,
+    fontSize: 23,
+  );
+  static const TextStyle regular22 = TextStyle(
+    fontFamily: 'SF Pro Display',
+    fontWeight: FontWeight.normal,
+    fontSize: 23,
+  );
+  static const TextStyle bold19 = TextStyle(
+    fontFamily: 'Cairo',
+    fontWeight: FontWeight.bold,
+    fontSize: 19,
+  );
+  static const TextStyle semiBold11 = TextStyle(
+    fontFamily: 'Cairo',
+    fontWeight: FontWeight.w600,
+    fontSize: 11,
+  );
+}
