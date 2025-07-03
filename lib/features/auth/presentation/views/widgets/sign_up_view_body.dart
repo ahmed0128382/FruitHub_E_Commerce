1a925cd90ddcf37e76ff89ecd9f923278e9aaac1
ahmed ibrahim mousa <ahmed0128382@gmail.com>
2025-07-04 00:18:57 +0300

    add text fields to sign up ui

--- lib/features/auth/presentation/views/widgets/sign_up_view_body.dart
+++ lib/features/auth/presentation/views/widgets/sign_up_view_body.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:fruit_hub/constants.dart';
+import 'package:fruit_hub/core/widgets/custom_text_form_field.dart';
 
 class SignUpViewBody extends StatelessWidget {
   const SignUpViewBody({super.key});
@@ -6,8 +8,36 @@ class SignUpViewBody extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return SingleChildScrollView(
-      child: Column(
-        children: [],
+      child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: kHorizontalPadding),
+        child: Column(
+          children: [
+            SizedBox(
+              height: 24,
+            ),
+            CustomTextFormField(
+                hintText: 'الاسم كامل', textInputType: TextInputType.name),
+            SizedBox(
+              height: 16,
+            ),
+            CustomTextFormField(
+                hintText: 'البريد الإلكتروني',
+                textInputType: TextInputType.emailAddress),
+            SizedBox(
+              height: 16,
+            ),
+            CustomTextFormField(
+                suffixIcon: Icon(
+                  Icons.remove_red_eye,
+                  color: Color(0xffc9cecf),
+                ),
+                hintText: 'كلمة المرور',
+                textInputType: TextInputType.visiblePassword),
+            SizedBox(
+              height: 16,
+            ),
+          ],
+        ),
       ),
     );
   }
