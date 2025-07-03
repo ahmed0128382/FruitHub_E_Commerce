72c2653b91d15fc1ebe266ff97cf2f858af70831
ahmed ibrahim mousa <ahmed0128382@gmail.com>
2025-07-03 23:38:38 +0300

    create the sign up view and its app bar and start its body

--- lib/features/auth/presentation/views/sign_up_view.dart
+++ lib/features/auth/presentation/views/sign_up_view.dart
@@ -1,10 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:fruit_hub/core/widgets/custom_app_bar.dart';
+import 'package:fruit_hub/features/auth/presentation/views/widgets/sign_up_view_body.dart';
 
 class SignUpView extends StatelessWidget {
   const SignUpView({super.key});
   static const routeName = 'register';
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: buildAppBar(context, title: 'حساب جديد'),
+      body: SignUpViewBody(),
+    );
   }
 }
