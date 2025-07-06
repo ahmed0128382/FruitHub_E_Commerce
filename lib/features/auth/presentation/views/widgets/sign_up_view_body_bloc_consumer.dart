415bdf60e7fb5fcc42fe4c45a7b1b822caef5a55
ahmed ibrahim mousa <ahmed0128382@gmail.com>
2025-07-06 15:53:47 +0300

    refactor build error bar

--- lib/features/auth/presentation/views/widgets/sign_up_view_body_bloc_consumer.dart
+++ lib/features/auth/presentation/views/widgets/sign_up_view_body_bloc_consumer.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:fruit_hub/core/helper/build_error_bar.dart';
 import 'package:fruit_hub/features/auth/presentation/manager/signup_cubit/signup_cubit.dart';
 import 'package:fruit_hub/features/auth/presentation/views/widgets/sign_up_view_body.dart';
 import 'package:modal_progress_hud_nsn/modal_progress_hud_nsn.dart';
@@ -17,8 +18,7 @@ class SignUpViewBodyBlocConsumer extends StatelessWidget {
           //Navigator.of(context).pop();
         }
         if (state is SignupFailure) {
-          ScaffoldMessenger.of(context)
-              .showSnackBar(SnackBar(content: Text(state.message)));
+          buildErrorBar(context, state.message);
         }
       },
       builder: (context, state) {
