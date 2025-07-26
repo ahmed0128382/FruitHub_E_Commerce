e88917182f3b19fa9b3f204fbd501c8ff6377764
ahmed ibrahim mousa <ahmed0128382@gmail.com>
2025-07-26 15:14:35 +0300

    replace provider of Provider package with MultiRepositoryProvider of flutter_bloc Package

--- lib/features/checkout/presentation/views/checkout_view.dart
+++ lib/features/checkout/presentation/views/checkout_view.dart
@@ -1,10 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:fruit_hub/core/widgets/custom_app_bar.dart';
 import 'package:fruit_hub/features/checkout/domain/entities/order_entity.dart';
 import 'package:fruit_hub/features/checkout/domain/entities/shipping_address_entity.dart';
 import 'package:fruit_hub/features/checkout/presentation/views/widgets/checkout_view_body.dart';
 import 'package:fruit_hub/features/main/domain/entities/carts_entity.dart';
-import 'package:provider/provider.dart';
 
 class CheckoutView extends StatelessWidget {
   const CheckoutView({super.key, required this.cartItems});
@@ -14,12 +14,14 @@ class CheckoutView extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: buildAppBar(context, title: 'الشحن'),
-      body: Provider.value(
-          value: OrderEntity(
+      body: MultiRepositoryProvider(providers: [
+        RepositoryProvider<OrderEntity>(
+          create: (_) => OrderEntity(
             shippingAddress: ShippingAddressEntity(),
             cartItems: cartItems,
           ),
-          child: CheckoutViewBody()),
+        ),
+      ], child: CheckoutViewBody()),
     );
   }
 }
