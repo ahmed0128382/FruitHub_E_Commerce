a54ddcfc7bb500d6d7bcdb2f0850662d1c2f3e9c
ahmed ibrahim mousa <ahmed0128382@gmail.com>
2025-07-27 16:50:59 +0300

    add paypal payment package to the project

--- lib/features/checkout/presentation/views/widgets/checkout_view_body.dart
+++ lib/features/checkout/presentation/views/widgets/checkout_view_body.dart
@@ -2,6 +2,7 @@ import 'dart:developer';
 
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_paypal_payment/flutter_paypal_payment.dart';
 import 'package:fruit_hub/constants.dart';
 import 'package:fruit_hub/core/helper/build_error_bar.dart';
 import 'package:fruit_hub/core/widgets/custom_button.dart';
@@ -138,5 +139,70 @@ class _CheckoutViewBodyState extends State<CheckoutViewBody> {
     }
   }
 
-  void handlePaymentValidation(BuildContext context) {}
+  void handlePaymentValidation(BuildContext context) {
+    Navigator.of(context).push(MaterialPageRoute(
+      builder: (BuildContext context) => PaypalCheckoutView(
+        sandboxMode: true,
+        clientId: "",
+        secretKey: "",
+        transactions: const [
+          {
+            "amount": {
+              "total": '70',
+              "currency": "USD",
+              "details": {
+                "subtotal": '70',
+                "shipping": '0',
+                "shipping_discount": 0
+              }
+            },
+            "description": "The payment transaction description.",
+            // "payment_options": {
+            //   "allowed_payment_method":
+            //       "INSTANT_FUNDING_SOURCE"
+            // },
+            "item_list": {
+              "items": [
+                {
+                  "name": "Apple",
+                  "quantity": 4,
+                  "price": '5',
+                  "currency": "USD"
+                },
+                {
+                  "name": "Pineapple",
+                  "quantity": 5,
+                  "price": '10',
+                  "currency": "USD"
+                }
+              ],
+
+              // shipping address is not required though
+              //   "shipping_address": {
+              //     "recipient_name": "tharwat",
+              //     "line1": "Alexandria",
+              //     "line2": "",
+              //     "city": "Alexandria",
+              //     "country_code": "EG",
+              //     "postal_code": "21505",
+              //     "phone": "+00000000",
+              //     "state": "Alexandria"
+              //  },
+            }
+          }
+        ],
+        note: "Contact us for any questions on your order.",
+        onSuccess: (Map params) async {
+          print("onSuccess: $params");
+        },
+        onError: (error) {
+          print("onError: $error");
+          Navigator.pop(context);
+        },
+        onCancel: () {
+          print('cancelled:');
+        },
+      ),
+    ));
+  }
 }
